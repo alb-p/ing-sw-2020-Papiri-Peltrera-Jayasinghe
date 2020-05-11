dfc57c3c8102f655ffaa468078906cda203e2e1a
sandroJaya <10530600@polimi.it>
2020-05-11 19:51:01 +0200

    new Pan.java

---
+++ src/main/java/it/polimi/ingsw/gods/Pan.java
@@ -0,0 +1,18 @@
+package it.polimi.ingsw.gods;
+
+import it.polimi.ingsw.model.BasicGodCard;
+import it.polimi.ingsw.model.IslandBoard;
+import it.polimi.ingsw.model.VirtualBoard;
+import it.polimi.ingsw.model.Worker;
+
+public class Pan extends BasicGodCard {
+
+    @Override
+    public boolean winningCondition(Worker w, IslandBoard board, VirtualBoard virtualBoard) {
+        if (virtualBoard.getSlot(w.getOldPosition().getRow(),w.getOldPosition().getCol()).getHeight() == 2 &&
+                board.infoSlot(w.getPosition()).getConstructionLevel() == 0) {
+            return true;
+        }
+        return super.winningCondition(w, board, virtualBoard);
+    }
+}
