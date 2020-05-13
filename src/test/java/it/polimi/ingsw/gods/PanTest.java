fd31d1287dcbb85196024da4b4d05b19b294a366
sandroJaya <10530600@polimi.it>
2020-05-14 00:09:39 +0200

    new ArtemisTest.java new PanTest.java

---
+++ src/test/java/it/polimi/ingsw/gods/PanTest.java
@@ -0,0 +1,41 @@
+package it.polimi.ingsw.gods;
+
+import it.polimi.ingsw.model.*;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PanTest {
+
+    BasicGodCard card = new Pan();
+    IslandBoard board = new IslandBoard();
+
+
+    @Before
+    public void init() throws Exception {
+
+
+        board.infoSlot(new Coordinate(2, 0)).construct(Construction.FLOOR);
+        board.infoSlot(new Coordinate(2, 0)).construct(Construction.FLOOR);
+
+    }
+
+    @Test
+    public void winningConditionTest() throws Exception {
+        Player player = new Player(0, "Pippo", Color.GRAY);
+        player.setCard("PAN");
+        player.selectWorker(new Coordinate(2, 0));
+        board.infoSlot(new Coordinate(2, 0)).occupy(player.getWorker(0));
+        player.getWorker(0).setPosition(new Coordinate(2, 0));
+
+
+        VirtualBoard virtualBoard=new VirtualBoard(board);
+
+
+        player.selectWorker(new Coordinate(2, 0));
+        assertTrue(card.turnHandler(player,board,new Move(new Coordinate(2,0),new Coordinate(2,1))));
+
+        assertTrue(card.winningCondition(player.getWorker(0),board,virtualBoard));
+    }
+}
