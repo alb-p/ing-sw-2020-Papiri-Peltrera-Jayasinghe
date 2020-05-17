7673deab887f8c732590b788507814f00e233f6e
gioelepeltrera <gioele.peltrera@mail.polimi.it>
2020-05-17 19:17:59 +0200

    TestTurnHandler Done.

---
+++ src/test/java/it/polimi/ingsw/controller/TurnHandlerTest.java
@@ -0,0 +1,105 @@
+package it.polimi.ingsw.controller;
+import it.polimi.ingsw.model.*;
+import it.polimi.ingsw.utils.messages.ActionMessage;
+import it.polimi.ingsw.utils.messages.Message;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.beans.PropertyChangeEvent;
+
+import static org.junit.Assert.*;
+
+public class TurnHandlerTest {
+
+    TurnHandler turnHandler;
+    TurnHandler turnHandler2;
+
+    @Before
+    public void init() throws CloneNotSupportedException {
+        Model model = new Model();
+        Player player = new Player(0, "Pluto", Color.BLUE);
+        model.addPlayer(player);
+        player = new Player(1, "Paperino", Color.GRAY);
+        model.addPlayer(player);
+        model.addWorker(0,new Coordinate(0,0),0);
+        model.addWorker(0,new Coordinate(0,4),1);
+        model.addWorker(1,new Coordinate(4,0),0);
+        model.addWorker(1,new Coordinate(4,4),1);
+        model.setCard(0,"Pan");
+        model.setCard(1,"Atlas");
+        turnHandler = new TurnHandler(model, 2);
+        turnHandler.setTotalTurnCounter(0);
+    }
+
+    @Before
+    public void initLockedPlayerMatch() throws CloneNotSupportedException {
+        Model model = new Model();
+        Player player = new Player(0, "Pluto", Color.BLUE);
+        model.addPlayer(player);
+        player = new Player(1, "Paperino", Color.GRAY);
+        model.addPlayer(player);
+        player = new Player(2, "Pippo", Color.WHITE);
+        model.addPlayer(player);
+        model.addWorker(0,new Coordinate(0,0),0);
+        model.addWorker(0,new Coordinate(0,1),1);
+        model.addWorker(1,new Coordinate(1,0),0);
+        model.addWorker(1,new Coordinate(1,1),1);
+        model.addWorker(2,new Coordinate(1,2),0);
+        model.addWorker(2,new Coordinate(0,2),1);
+        model.setCard(0,"Pan");
+        model.setCard(1,"Atlas");
+        model.setCard(1,"Athena");
+        turnHandler2 = new TurnHandler(model, 3);
+        turnHandler.setTotalTurnCounter(0);
+    }
+
+    @Test
+    public void propertyChangeTest(){
+        ActionMessage message;
+        message = new ActionMessage(0, "Pluto");
+        message.setAction(new Move(new Coordinate(0,0),new Coordinate(0,1)));
+        PropertyChangeEvent evt = new PropertyChangeEvent(this, "gameReadyResponse",
+                null , message);
+
+        turnHandler.propertyChange(evt);
+
+        evt = new PropertyChangeEvent(this, "actionMessageResponse",
+                null , message);
+
+        turnHandler.propertyChange(evt);
+        assertEquals(0,turnHandler.actualPlayer());
+
+        message.setAction(new Build(new Coordinate(1,1),new Coordinate(0,0)));
+        evt = new PropertyChangeEvent(this, "actionMessageResponse",
+                null , message);
+        turnHandler.propertyChange(evt);
+
+        assertEquals(0,turnHandler.actualPlayer());
+
+        message.setAction(new Build(new Coordinate(0,1),new Coordinate(4,0)));
+        evt = new PropertyChangeEvent(this, "actionMessageResponse",
+                null , message);
+        turnHandler.propertyChange(evt);
+
+        assertEquals(0,turnHandler.actualPlayer());
+
+
+        message.setAction(new Build(new Coordinate(0,1),new Coordinate(0,0)));
+
+        evt = new PropertyChangeEvent(this, "actionMessageResponse",
+                null , message);
+        turnHandler.propertyChange(evt);
+
+        assertEquals(1,turnHandler.actualPlayer());
+
+    }
+
+    @Test
+    public void playerHasLostTest(){
+        turnHandler2.propertyChange(new PropertyChangeEvent(this,"gameReadyResponse",null, true));
+        assertEquals(1, turnHandler2.actualPlayer());
+    }
+
+
+
+}
